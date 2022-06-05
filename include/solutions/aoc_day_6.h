b626289417eb678dac533bf10f550876abb780f1
Joey Territo <jtt9340@rit.edu>
2022-06-05 16:43:17 -0400

    Day 6 Part 2

--- include/solutions/aoc_day_6.h
+++ include/solutions/aoc_day_6.h
@@ -1,15 +1,43 @@
+#include <array>
+
 #include "aoc_days.h"
 
 #ifndef __AOC_DAY_6__
 #define __AOC_DAY_6__
 
-constexpr size_t NUM_DAYS = 80;
+class lanternfish {
+	using record = array<size_t, 9>;
+	record timers;
+
+	void next_day();
+public:
+	template <class InputIt>
+	lanternfish(InputIt begin, InputIt end)
+		: timers{0, 0, 0, 0, 0, 0, 0, 0, 0}
+	{
+		for (auto it = begin; it != end; it++)
+		{
+			try
+			{
+				timers.at(*it)++;
+			}
+			catch (out_of_range &e)
+			{
+				throw invalid_argument{"A fish's count cannot be outside the range [0, 8]"};
+			}
+		}
+	}
+
+	size_t simulate(const size_t days);
+};
 
 class AocDay6 : public AocDay {
+	vector<long> read_input(string &filename);
 public:
 	AocDay6();
 	~AocDay6();
 	string part1(string &filename, vector<string> &extra_args) override;
+	string part2(string &filename, vector<string> &extra_args) override;
 };
 
 #endif
--- src/solutions/aoc_day_6.cpp
+++ src/solutions/aoc_day_6.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cassert>
+#include <numeric>
 
 #include "aoc_day_6.h"
 #include "file_utils.h"
@@ -8,51 +9,32 @@
 
 using namespace std;
 
-#ifdef DEBUG_OTHER
-struct report_day
+void lanternfish::next_day()
 {
-	const size_t day;
-	const vector<long> &lanternfish;
-
-	report_day(const size_t n, const vector<long> &timers)
-		: day{n}, lanternfish{timers} {}
-};
-
-template <class T>
-static ostream &operator<<(ostream &os, const vector<T> &values)
-{
-	for (auto it = begin(values); it != end(values); it++)
+	lanternfish::record transaction{timers};
+	for (int8_t i = timers.size() - 1; i >= 0; i--)
 	{
-		os << *it;
-		if (it + 2 != end(values))
-			os << ',';
-	}
-	return os;
-}
-
-static ostream &operator<<(ostream &out, report_day curr)
-{
-	return out << "After" << setw(3) << curr.day
-		       << (curr.day == 1 ? "day: " : " days: ")
-			   << curr.lanternfish; 
-}
-#endif
-
-static void next_day(vector<long> &lanternfish)
-{
-	const auto cur_size = lanternfish.size();
-	for (auto i = 0; i < cur_size; i++)
-	{
-		if (lanternfish[i] == 0)
+		const auto count = timers[i];
+		if (i == 0)
 		{
-			lanternfish[i] = 6;
-			lanternfish.push_back(8);
+			transaction[i] -= count;
+			transaction[6] += count;
+			transaction[8] += count;
 		}
 		else
 		{
-			lanternfish[i]--;
+			transaction[i] -= count;
+			transaction[i - 1] += count;
 		}
 	}
+	timers = transaction;
+}
+
+size_t lanternfish::simulate(const size_t days)
+{
+	for (auto i = 0; i < days; i++)
+		next_day();
+	return accumulate(begin(timers) + 1, end(timers), timers[0]);
 }
 
 AocDay6::AocDay6():AocDay(6)
@@ -63,7 +45,7 @@ AocDay6::~AocDay6()
 {
 }
 
-string AocDay6::part1(string &filename, vector<string> &extra_args)
+vector<long> AocDay6::read_input(string &filename)
 {
 	FileUtils fileutils;
 	vector<vector<long>> lanternfish;
@@ -71,21 +53,23 @@ string AocDay6::part1(string &filename, vector<string> &extra_args)
 	if (!fileutils.read_as_list_of_split_longs(filename, lanternfish, TEST_INDEX_DELIM, TEST_INDEX_QUOTE, TEST_INDEX_COMMENT))
 	{
 		cerr << "Error reading in the data from " << filename << endl;
-		return "";
+		return {};
 	}
 
 	assert(lanternfish.size() == 1);
-	vector<long> &timers = lanternfish[0];
-#ifdef DEBUG_OTHER
-	cout << "Initial state: " << timers << endl;
-#endif
-	for (auto i = 0; i < NUM_DAYS; i++)
-	{
-		next_day(timers);
-#ifdef DEBUG_OTHER
-		cout << report_day(i + 1, timers) << '\n';
-#endif
-	}
+	return move(lanternfish[0]);
+}
 
-	return to_string(timers.size());
+string AocDay6::part1(string &filename, vector<string> &extra_args)
+{
+	auto timers = read_input(filename);
+	lanternfish solution{begin(timers), end(timers)};
+	return to_string(solution.simulate(80));
+}
+
+string AocDay6::part2(string &filename, vector<string> &extra_args)
+{
+	auto timers = read_input(filename);
+	lanternfish solution{begin(timers), end(timers)};
+	return to_string(solution.simulate(256));
 }
